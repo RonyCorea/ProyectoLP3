a8474364fe55bb08b8dbaffd4794fc92ca89c52f
GenesisFunes <genesis.funes@unah.com>
2022-04-13 21:32:04 -0600

    Avance Genesis
    Avance Genesis

---
+++ PoryectoBlazor/ProyectoBlazor/Pages/Productos/EditarProducto.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoBlazor.Interfaces;
+
+namespace ProyectoBlazor.Pages.Productos;
+
+partial class EditarProducto
+{
+    
+    [Inject] private IProductoServicio _productoServicio { get; set; }
+
+    [Parameter]public string Codigo { get; set; }
+
+    Producto product = new Producto();
+
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (!string.IsNullOrEmpty(Codigo))
+        {
+            product = await _productoServicio.GetPorCodigo(Codigo);
+        }
+        
+       
+    }
+}
