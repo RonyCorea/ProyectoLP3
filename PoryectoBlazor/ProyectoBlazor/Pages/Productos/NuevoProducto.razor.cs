9dea917f8a373624e8883ebb215c2a500ee0c92a
MaryanyGuillen <maryany.guillen@unah.hn>
2022-04-20 11:54:12 -0600

    Avance Maryany
    AvanceMaryany

--- PoryectoBlazor/ProyectoBlazor/Pages/Productos/NuevoProducto.razor.cs
+++ PoryectoBlazor/ProyectoBlazor/Pages/Productos/NuevoProducto.razor.cs
@@ -1,6 +1,40 @@
-﻿namespace ProyectoBlazor.Pages.Productos
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoBlazor.Interfaces;
+
+namespace ProyectoBlazor.Pages.Productos
 {
     partial class NuevoProducto
     {
+        [Inject] private IProductoServicio productoServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        [Inject] SweetAlertService Swal { get; set; }
+
+        private Producto product = new Producto();
+
+        protected async Task Guardar()
+        {
+            if (string.IsNullOrEmpty(product.Codigo) || string.IsNullOrEmpty(product.Descripcion))
+            {
+                return;
+            }
+
+            bool edito = await productoServicio.Actualizar(product);
+            if (edito)
+            {
+                await Swal.FireAsync("Felicidades", "Producto actualizado con éxito", SweetAlertIcon.Success);
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "Producto no pudo ser actualizado", SweetAlertIcon.Error);
+            }
+
+            navigationManager.NavigateTo("/Productos");
+        }
+        protected async Task Cancelar()
+        {
+            navigationManager.NavigateTo("/Productos");
+        }
     }
 }
