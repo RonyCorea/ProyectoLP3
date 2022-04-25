1f24b3d779e332cf1a9219df134c4186a5f894fe
RonyCorea <fabriciosuazo.2001@gmail.com>
2022-04-24 22:43:20 -0600

    NuevaCompra...

--- PoryectoBlazor/ProyectoBlazor/Pages/Compras/ComprarProducto.razor.cs
+++ PoryectoBlazor/ProyectoBlazor/Pages/Compras/ComprarProducto.razor.cs
@@ -1,5 +1,18 @@
-﻿namespace ProyectoBlazor.Pages.Compra;
+﻿using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoBlazor.Interfaces;
 
-partial class ComprarProducto
+namespace ProyectoBlazor.Pages.Compras
 {
+    partial class ComprarProducto
+    {
+        [Inject] private ICompraServicio _compraServicio { get; set; }
+
+        private IEnumerable<Compra> comprasLista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            comprasLista = await _compraServicio.GetLista();
+        }
+    }
 }
---
+++ PoryectoBlazor/ProyectoBlazor/Pages/Compras/NuevaCompra.razor.cs
@@ -0,0 +1,40 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoBlazor.Interfaces;
+
+namespace ProyectoBlazor.Pages.Compras
+{
+    partial class NuevaCompra
+    {
+        [Inject] private ICompraServicio compraServicio { get; set; }
+        [Inject] private NavigationManager navigationManager { get; set; }
+        [Inject] SweetAlertService Swal { get; set; }
+
+        private Compra compra = new Compra();
+
+        protected async Task Comprar()
+        {
+            if (compra.Id==null||string.IsNullOrEmpty(compra.Cliente)|| string.IsNullOrEmpty(compra.CodigoProducto))
+            {
+                return;
+            }
+
+            bool inserto = await compraServicio.Comprar(compra);
+            if (inserto)
+            {
+                await Swal.FireAsync("Felicidades", "Compra realizada con éxito", SweetAlertIcon.Success);
+            }
+            else
+            {
+                await Swal.FireAsync("Error", "No se pudo realizar la compra", SweetAlertIcon.Error);
+            }
+
+            navigationManager.NavigateTo("/Compra");
+        }
+        protected async Task Cancelar()
+        {
+            navigationManager.NavigateTo("/Compra");
+        }
+    }
+}
