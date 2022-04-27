8795ac3edd3a7388d4e2327d4260c1ab2c2a9d48
RonyCorea <fabriciosuazo.2001@gmail.com>
2022-04-26 18:09:22 -0600

    ListaDeCompras

---
+++ PoryectoBlazor/ProyectoBlazor/Pages/Compras/Registros.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Modelos;
+using ProyectoBlazor.Interfaces;
+
+namespace ProyectoBlazor.Pages.Compras
+{
+    partial class Registros
+    {
+        [Inject] private ICompraServicio _compraServicio { get; set; }
+
+        private IEnumerable<Compra> comprasLista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            comprasLista = await _compraServicio.GetLista();
+        }
+    }
+}
